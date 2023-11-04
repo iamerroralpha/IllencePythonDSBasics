46163e2e23cff58e6fa4bdd2474d85c71a15ef49
Gubynator <goubiah@gmail.com>
2023-11-04 13:02:48 -0600

    añadir ejercicios

--- ejercicios_no_resueltos.py
+++ ejercicios_no_resueltos.py
@@ -51,4 +51,42 @@ def par_o_impar(numero):
 lista = [3, 1, 4, 1, 5, 9, 2, 6, 5]
 print(lista[1:5])
 
-# %%
+# 10. Crea una función que retorne los números pares de una lista dada.# %%
+
+# 11 Genera una lista con los números cuadrados del 1 al 100... Si utilizas list comprehension, eres un Jedi.
+
+# 12 Crea una función que reciba una cadena y retorne la misma cadena de caracteres invertida.
+
+#"Hola que hace" => "ecah euq aloH"
+
+# 13. Usa un bucle while para imprimir los números del 5 al 1 en orden descendente.
+
+# 14. Crea una función que determine si una palabra es un palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda).
+
+# 15. Escribe una función que cuente la cantidad de veces que aparece una letra en una cadena.
+def contar_letra(cadena, letra):
+    '''
+    Cadena = "Cadena de prueba"
+    letra = "a"
+    resultado = 3 
+    '''
+
+# 16. Crea una función que retorne el número mayor de una lista. Eres un jedi si lo haces con numpy.
+
+# 17. Escribe una función que multiplique todos los números de una lista.
+
+# 18. Crea una función que reciba una lista de palabras y retorne la palabra más larga.
+
+def lista_mas_larga(lista):
+    '''
+    lista = ["I", "love", "python"]
+    resultado = python
+    '''
+
+# 19. Escribe una función que determine si una lista tiene elementos duplicados. True si tiene duplicados False si no
+# [1,2,3,4,5,6,"Hola"] => False
+# [1,2,3,4,5,6,"Hola","Hola"] => True
+
+# 20. Crea una función que remueva todos los elementos duplicados de una lista.
+# [1,2,3,4,5,6,"Hola"] => [1,2,3,4,5,6,"Hola"]
+# [1,2,3,4,5,6,"Hola","Hola"] => [1,2,3,4,5,6,"Hola"]
